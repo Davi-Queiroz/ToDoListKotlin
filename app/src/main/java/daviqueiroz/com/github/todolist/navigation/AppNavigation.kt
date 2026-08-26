f129d0779f73d2e6895191e26deb6849bbe22dfd
Davi Queiroz Santos <davi.santos@eval.digital>
2026-08-26 10:11:43 -0300

    Add ordenacao por hora

--- app/src/main/java/daviqueiroz/com/github/todolist/navigation/AppNavigation.kt
+++ app/src/main/java/daviqueiroz/com/github/todolist/navigation/AppNavigation.kt
@@ -1,4 +1,32 @@
 package daviqueiroz.com.github.todolist.navigation
 
-class AppNavigation {
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import daviqueiroz.com.github.todolist.ui.ListaTarefasScreen
+import daviqueiroz.com.github.todolist.viewmodel.TarefaViewModel
+import daviqueiroz.com.github.todolist.ui.FormularioTarefaScreen
+
+@Composable
+fun AppNavigation(viewModel: TarefaViewModel) {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "lista") {
+        composable("lista") {
+            ListaTarefasScreen(
+                viewModel = viewModel,
+                onNovaTarefa = { navController.navigate("formulario/0") },
+                onEditarTarefa = { id -> navController.navigate("formulario/$id") }
+            )
+        }
+        composable("formulario/{tarefaId}") { backStackEntry ->
+            val tarefaId = backStackEntry.arguments?.getString("tarefaId")?.toInt() ?: 0
+            FormularioTarefaScreen(
+                viewModel = viewModel,
+                tarefaId = tarefaId,
+                onVoltar = { navController.popBackStack() }
+            )
+        }
+    }
 }
